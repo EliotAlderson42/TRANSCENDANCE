59c09e0d9724e583e8e036ba3c80b5cd1101c0ca
Malarea <emrimachreki@gmail.com>
2025-01-08 18:08:17 +0100

    nettoyage

--- backend/pong/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import pong_view
-
-urlpatterns = [
-    path('pong/', pong_view, name='pong_view'),  # URL pour accéder au jeu Pong
-]
--- backend/pong/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-def pong_view(request):
-    return render(request, 'pong/index.html')  # Rendre le template HTML pour le jeu Pong
